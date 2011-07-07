5b87ed2b7e1322da9ced4429cd7e4853ea810590
Rob Blackwell <rob.blackwell@aws.net>
2011-07-07 12:02:25 +0100

    Updated assemblyinfo

--- TraceProcess/Properties/AssemblyInfo.cs
+++ TraceProcess/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TraceProcess")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Active Web Solutions")]
 [assembly: AssemblyProduct("TraceProcess")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Active Web Solutions 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
